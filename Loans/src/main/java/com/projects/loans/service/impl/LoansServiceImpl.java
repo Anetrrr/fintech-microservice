2484952443f046836621f454ea11522cffad81d5
Anetor <anetor.ataga@gmail.com>
2024-07-06 08:44:06 +0100

    structured out loans microservice

---
+++ Loans/src/main/java/com/projects/loans/service/impl/LoansServiceImpl.java
@@ -0,0 +1,4 @@
+package com.projects.loans.service.impl;
+
+public class LoansServiceImpl {
+}
---
+++ Loans/src/test/java/com/projects/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
